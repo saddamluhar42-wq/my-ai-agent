5a19005d8765006101774ed2909e6595d9e976f0
sadamluhar-collab <sadamluhar@gmail.com>
2026-08-11 13:00:33 +0530

    Create __init__.py

---
+++ skills/__init__.py
@@ -0,0 +1,25 @@
+from skills.registry import (
+    SkillDefinition,
+    SkillRegistry,
+    disable_skill,
+    enable_skill,
+    get_all_skills,
+    get_enabled_skills,
+    get_skill,
+    get_skill_names,
+    register_skill,
+    registry,
+)
+
+__all__ = [
+    "SkillDefinition",
+    "SkillRegistry",
+    "registry",
+    "get_skill",
+    "get_all_skills",
+    "get_enabled_skills",
+    "get_skill_names",
+    "register_skill",
+    "enable_skill",
+    "disable_skill",
+]
